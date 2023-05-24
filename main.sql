65727223a6db056f3c9750d0d214206cea32d3e6
brr26 <134021303+brr26@users.noreply.github.com>
2023-05-24 20:56:59 -0300

    main.sql

--- main.sql
+++ main.sql
@@ -1,4 +1,3 @@
--- create a table for products
 CREATE TABLE products (
   id INT(11) NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
@@ -7,7 +6,6 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
--- create a table for companies
 CREATE TABLE companies (
   id INT(11) NOT NULL AUTO_INCREMENT,
   cnpj VARCHAR(14) NOT NULL,
@@ -15,10 +13,8 @@ CREATE TABLE companies (
   PRIMARY KEY (id)
 );
 
--- add a product to the products table
 INSERT INTO products (id, name, value, status)
 VALUES (1, 'Product 1', 10.99, 'active');
 
--- add a company to the companies table
 INSERT INTO companies (id, cnpj, status)
 VALUES (1, '12345678901234', 'active');
